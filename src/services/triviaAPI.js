217a62ac836a70f5d4562ff91815e706a0025ca5
danielfasanaro <danielfasanaro@gmail.com>
2021-05-10 19:13:23 -0300

    feat(API): Add triviaAPI

---
+++ src/services/triviaAPI.js
@@ -0,0 +1,23 @@
+export async function fetchNewToken() {
+  try {
+    const endpoint = 'https://opentdb.com/api_token.php?command=request';
+    const fetchResponse = await fetch(endpoint);
+    if (!fetchResponse.ok) throw new Error(fetchResponse.statusTexte);
+    const fetchResult = await fetchResponse.json();
+    return fetchResult;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function fetchQuestions(token) {
+  try {
+    const endpoint = `https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean&token=${token}`;
+    const fetchResponse = await fetch(endpoint);
+    if (!fetchResponse.ok) throw new Error(fetchResponse.statusText);
+    const fetchResult = await fetchResponse.json();
+    return fetchResult.results;
+  } catch (err) {
+    console.error(err);
+  }
+}
